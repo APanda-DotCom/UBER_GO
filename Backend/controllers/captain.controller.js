6da87352157525b51180c924ec195aa9a994d72d
APanda-DotCom <ashutoshpanda843@gmail.com>
2025-12-28 23:12:04 +0530

    Add captain.controller

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,2 +1,7 @@
 const  captainmodel = require('../models/captain.model');
 const captainService =require('../services/captain.service');
+const {validationResult}=require('express-validator');
+
+module.exports.registerCaptian = async (req,res,next)=>{
+    
+}
