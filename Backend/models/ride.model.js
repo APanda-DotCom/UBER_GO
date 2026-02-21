35a4abdc8d6a03f4b5fbf6782d4e8a49686cdb9a
APanda-DotCom <ashutoshpanda843@gmail.com>
2026-02-21 16:23:43 +0530

    Backend add ride model

--- Backend/models/ride.model.js
+++ Backend/models/ride.model.js
@@ -1,50 +1,50 @@
 const mongoose = require('mongoose');
 
 const rideSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-        required: true
-    },
-    captain: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'captain',
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
 
-    },
-    pickup:{
-        type:String,
-        required:true,
-    },
-    destination:{
-        type:String,
-        required:true,
-    
-    },
-    fare:{
-        type:Number,
-        required:true,
-    },
-    status:{
-        type:String,
-        enum:['pending','accepted','ongoing','completed','cancelled'],
-        default:'pending',
-    },
-    duration:{
-        type:Number,
-    },
-    distance:{
-        type:Number,
-    },
-    paymentId:{
-      type:String,
-    },
-    orderId:{
-        type:String,
-    },
-    signature:{
-        type:String,
-    },  
+  captain: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'captain', 
+  },
 
-})
+  pickup: {
+    type: String,
+    required: true,
+  },
 
-module.exports = mongoose.model('ride',rideSchema)
+  destination: {
+    type: String,
+    required: true,
+  },
+
+  fare: {
+    type: Number,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'ongoing', 'completed', 'cancelled'],
+    default: 'pending',
+  },
+
+  duration: Number,
+  distance: Number,
+
+  paymentId: String,
+  orderId: String,
+  signature: String,
+
+  otp: {
+    type: String,
+    select: false,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('Ride', rideSchema);
