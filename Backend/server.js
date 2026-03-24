83c8d064508f2cefe6de115fa7003e47e4e5fda6
APanda-DotCom <ashutoshpanda843@gmail.com>
2026-03-24 22:20:36 +0530

    Add BAckend Server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const app = require('./app');
 
-
+const {initializeSocket}=require('./socket')
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-
+// initialize socket.io with the created HTTP server
+initializeSocket(server);
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
